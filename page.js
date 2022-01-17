bbe1251f848e8b6a581bf8a91dca7e89ce344ce6
joe <hudakjoe@msn.com>
2022-01-16 23:36:41 -0500

    does not work :(

--- page.js
+++ page.js
@@ -55,14 +55,16 @@ const generateTeam = (team) => {
     `;
     html.push(internHtml);
   };
-
+  console.log(team.length);
   for (let i = 0; i < team.length; i++) {
     if (team[i].getRole() === 'Manager') {
       generateManager(team[i]);
     } else if (team[i].getRole() === 'Engineer') {
       generateEngineer(team[i]);
     } else if (team[i].getRole() === 'Intern') {
+      console.log(team[i]);
       generateIntern(team[i]);
+      console.log(html);
     }
   }
 
@@ -81,12 +83,12 @@ module.exports = (team) => {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Portfolio Demo</title>
+  <title>Teams</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="import" href="style.css" type="text/css">
   <link rel="stylesheet" href="style.css">
-  <script src="https://kit.fontawesome.com/102433624d.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/6531e1e821.js" crossorigin="anonymous"></script>
 </head>
   
   <body>
